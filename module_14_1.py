727e326a5fd00760a54fb1761567040d94b73d44
IvanGitHub7 <usoltsev-ivan94@mail.ru>
2024-11-24 18:13:38 +0700

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Users'")
+if cursor.fetchone() is None:
+    for i in range(1, 11):
+        cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                       (f"User{i}", f"example{i}@gmail.com", f"{10 * i}", "1000"))
+
+# for i in range(1, 11):
+#     if i % 2:
+#         cursor.execute("UPDATE Users SET balance = ? WHERE username = ?",
+#                        ("500", f"User{i}"))
+
+# for i in range (1, 11, 3):
+#     cursor.execute("DELETE FROM Users WHERE id = ?", (str(i),))
+#
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60 ORDER BY AGE ASC")
+users = cursor.fetchall()
+for user in users:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+
+connection.commit()
+connection.close()
